fa347ec6c2420b686d5c056e24a84ce2c0b96e8d
Dastan <eles@MacBook-Air-Dastan.local>
2023-02-13 21:38:18 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const styles = {
+  app: 'text-center bg-lime-900	h-screen w-4/5',
+  h1: 'text-red-500',
+};
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <h1 className={styles.h1}>Ruslan yebishe</h1>
+    </div>
+  );
+}
+
+export default App;
