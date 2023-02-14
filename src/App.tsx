a37a68cfb61d99fe9ce20af75cfecec1abb5f476
Dastan <eles@MacBook-Air-Dastan.local>
2023-02-14 21:24:46 +0600

    add firebase with config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Header from './components/Header';
+import { logo } from './assets';
+import { FirebaseDb } from './db/db';
 
 const styles = {
-  app: 'text-center bg-lime-900	h-screen w-4/5',
+  app: 'text-center',
   h1: 'text-red-500',
 };
 
+const db = new FirebaseDb();
+
 function App() {
+  useEffect(() => {
+    db.getProducts()
+  }, []);
   return (
     <div className={styles.app}>
+      <Header logo={logo} />
       <h1 className={styles.h1}>Ruslan yebishe</h1>
     </div>
   );
---
+++ src/config/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBsMYIQyW2POCymZilwiaCLat5W_8hhAL4',
+  authDomain: 'onlineshop-f585e.firebaseapp.com',
+  projectId: 'onlineshop-f585e',
+  storageBucket: 'onlineshop-f585e.appspot.com',
+  messagingSenderId: '882584609428',
+  appId: '1:882584609428:web:8ecc97a798f102ebf84a81',
+};
